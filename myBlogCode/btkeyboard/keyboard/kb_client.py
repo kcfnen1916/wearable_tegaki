a81a3f199d2f9762cca6f1e03cb61c2c51f8c260
Yuki Sano <yukidaruma-cl37@i.softbank.jp>
2019-08-20 17:53:47 +0900

    change print

--- myBlogCode/btkeyboard/keyboard/kb_client.py
+++ myBlogCode/btkeyboard/keyboard/kb_client.py
@@ -46,14 +46,14 @@ class Keyboard():
 			0x00,
 			0x00]
 
-		#print "setting up DBus Client"
+		print("setting up DBus Client")
 
 		self.bus = dbus.SystemBus()
 		self.btkservice = self.bus.get_object('org.yaptb.btkbservice','/org/yaptb/btkbservice')
 		self.iface = dbus.Interface(self.btkservice,'org.yaptb.btkbservice')
 
 
-		#print "waiting for keyboard"
+		print("waiting for keyboard")
 
 		#keep trying to key a keyboard
 		have_dev=False
@@ -64,9 +64,9 @@ class Keyboard():
 				self.dev = InputDevice("/dev/input/event0")
 				have_dev=True
 			except OSError:
-				#print "Keyboard not found, waiting 3 seconds and retrying"
+				print("Keyboard not found, waiting 3 seconds and retrying")
 				time.sleep(3)
-			#print "found a keyboard"
+			print("found a keyboard")
 
 
 
@@ -116,10 +116,10 @@ class Keyboard():
 
 if __name__ == "__main__":
 
-	#print "Setting up keyboard"
+	print("Setting up keyboard")
 
 	kb = Keyboard()
 
-	#print "starting event loop"
+	print("starting event loop")
 	kb.event_loop()
 
