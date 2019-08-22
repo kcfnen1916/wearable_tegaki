5d8455560e92f4580b0d26cd4f60303a912dbccc
Yuki Sano <yukidaruma-cl37@i.softbank.jp>
2019-08-22 17:32:52 +0900

    change number

--- myBlogCode/btkeyboard/keyboard/kb_client.py
+++ myBlogCode/btkeyboard/keyboard/kb_client.py
@@ -105,12 +105,13 @@ class Keyboard():
 
 
 	#forward keyboard events to the dbus service
+	
 	def send_input(self):
 		bin_str=""
 		element=self.state[2]
 		for bit in element:
 			bin_str += str(bit)
-		self.iface.send_keys(int(bin_str,2),self.state[4:10]  )
+		self.iface.send_keys(int(bin_str,2),self.state[4:10])
 
 
 
