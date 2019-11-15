ca24a6db8160b142a89a5f26cfcedb71310a6df6
gedatsu217 <zais712@yahoo.co.jp>
2019-11-15 17:19:26 +0900

    make config_to_c.py

---
+++ esp32/config_to_c.py
@@ -0,0 +1,41 @@
+def translation(dic, list):
+    export_translation=""
+    list = (list.strip("[]")).split(",")
+    for i in list:
+        export_translation += dic[i]
+
+    return export_translation
+        
+
+
+file = open("gesture.config", "r")
+export_file = open("new_config.cpp" ,"w")
+
+dic={}
+for line in file:
+    export = ""
+    line=line.replace(", ",",")
+    line = line.split()
+    try:
+        if (line[0]=="Key"):
+            dic[line[1]]=line[3]
+            true_or_false="false"
+            if(line[3]=="d" or line[3]=="s" or line[3]=="c"):
+                true_or_false="true"
+            export += line[0]+" "+line[1]+" = "+line[0]+"("+line[2]+', "'+line[1]+'", "'+line[3]+'", wearbo, '+true_or_false+");"
+            export_file.write(export)
+            export_file.write("\n")
+
+        if(line[0]=="Gesture"):
+            if(line[1]=="spc"):
+                line[3] = '" "'
+                line[4] = line[5]
+            export += line[0]+" "+line[1]+" = "+line[0]+'("'+translation(dic, line[2])+'", '+str(len((line[2].strip("[]")).split(",")))+", "+line[3]+", "+line[4]+");"
+            export_file.write(export)
+            export_file.write("\n")
+    except IndexError:
+        export_file.write("\n")
+
+
+file.close()
+export_file.close()
